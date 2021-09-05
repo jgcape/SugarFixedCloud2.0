e585d20df67de75a23be84a9fc8f0b79f3a2de8f
Kahlia <kahlia.hogg@gmail.com>
2021-09-05 15:01:07 +1000

    Fix image preview

--- public/js/env.js
+++ public/js/env.js
@@ -10,17 +10,42 @@ const allowCamera = () => {
     };
 }
 
-const cameraPreview = () => {
+const takePhoto = () => {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
-}
-
-
-const takePhoto =() => {
     var video = document.getElementById('video');
     context.drawImage(video, 0, 0, 640, 480);
+    var img = canvas.toBlob()
 }
 
+const uploadImage = (bootData) => {
+    $.ajax({
+        enctype:'multipart/form-data',
+        url: '/api/boots',
+        data: bootData,
+        type: 'POST',
+        contentType:false,
+        processData:false,
+        success: (result) => {
+            $('#modal1').modal('close')
+            $('#queryBoot').attr("src", result.imgURL).toggle(true);
+            showResults(result.preds);
+        },
+        error: (err) => {
+            alert(err.message);
+            // location.reload();
+        }
+    })
+}
+
+const submitImg = () => {
+    // let labelImg = $('#canvas')canvas.toBlob;
+    var formData = new FormData();
+    formData.append("bootImg", bootImg);
+
+    console.log("Form Data Submitted: ", formData);
+    uploadBoot(formData);
+}
 
 $(document).ready(function(){
     console.log('Ready');
@@ -29,11 +54,14 @@ $(document).ready(function(){
     $("#labelForm").click(()=>{
         $('.modal').modal('open');
         allowCamera();
-        cameraPreview();
     })
 
     $('#snap').click(()=>{
         takePhoto()
+    });
+
+    $('#formSubmit').click(()=>{
+        submitImg();
     });
   
     
