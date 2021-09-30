1d740d231242b3b16086f75a032616eb8379aac7
Kahlia <kahlia.hogg@gmail.com>
2021-09-30 17:27:06 +1000

    Display result data to results page
    Minimum results structure

--- public/js/results.js
+++ public/js/results.js
@@ -1,20 +1,51 @@
-const getResult = () => {
-    // console.log("Querying", userID);
+const createCards = (allResults) => {
+    allResults.forEach(result => {
+        console.log(result)
+        let item='<div class="card col l4">'+
+      '<div class="card-content">'+
+        '<span class="card-title grey-text text-darken-4">'+result.productName+'</span>'+
+        '<p>'+result.date+'</p>'+
+        '<p>'+result.sugars+'</p>'+
+        '<button id='+result._id+'class="btn waves-effect waves-red btn-large">Delete</button>'+
+        '<button id='+result._id+'class="btn waves-effect waves-red btn-large">Update</button>'+
+      '</div>'+
+    '</div>'          
+    
+    $('#resultCards').append(item)
+    });
+};
+
+const getAllResults = () => {
     $.ajax({
-        url: '/api/sugars/latest',
+        url: '/api/sugars/',
         type: 'GET',
         success: (result) => {
-            console.log(result)
-            $('#result').html(result.data)
+            console.log(result.data)
+            createCards(result.data)
         },
         error: (err) => {
             alert(err.message);
         }
-    })
+    });
+};
+
+const getLatestResult = () => {
+    $.ajax({
+        url: '/api/sugars/latest/',
+        type: 'GET',
+        success: (result) => {
+            console.log(result.data)
+            $('#result').html(result.data.sugars)
+        },
+        error: (err) => {
+            alert(err.message);
+        }
+    });
 };
 
 
 $(document).ready(function(){
     console.log('Ready');
-    getResult()
+    getAllResults()
+    getLatestResult()
   });
--- routes/sugars.js
+++ routes/sugars.js
@@ -3,9 +3,6 @@ var express = require("express");
 var router = express.Router();
 var Controllers = require("../controllers");
 
-// // Load Sugar model
-// const Sugar = require('../models/Sugars');
-
 router.get('/', (req, res) => {
     var userID = req._passport['session']['user']
     Controllers.sugarsController.getUserSugars(userID, res);
@@ -14,14 +11,6 @@ router.get('/', (req, res) => {
 router.get('/latest', (req, res) => {
     var userID = req._passport['session']['user']
     Controllers.sugarsController.getUserResult(userID, res);
-    
-    // Sugar.findOne({userID: userID}).sort({date: -1}).exec(function(err, result) { 
-    //     res.json({
-    //         statusCode:200,
-    //         data: result.sugars,
-    //         message: "Sucess: retrieved latest result"
-    //     });
-    // });
 });
 
 module.exports = router;
